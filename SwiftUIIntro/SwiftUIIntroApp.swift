31979907f182b2742cd5eb6d0d06fecd8f0160ea
Osman Taha Deniz <osmancobhc@gmail.com>
2023-04-17 12:38:21 +0300

    Initial Commit

---
+++ SwiftUIIntro/SwiftUIIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIntroApp.swift
+//  SwiftUIIntro
+//
+//  Created by Osman Taha Deniz on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
