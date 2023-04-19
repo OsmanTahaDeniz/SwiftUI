74c406be77b600670a88af42c601503ef12b7a73
Osman Taha Deniz <osmancobhc@gmail.com>
2023-04-19 09:42:13 +0300

    Icons added

---
+++ SwiftUIIntro/Icons.swift
@@ -0,0 +1,30 @@
+//
+//  Icons.swift
+//  SwiftUIIntro
+//
+//  Created by Osman Taha Deniz on 18.04.2023.
+//
+
+import SwiftUI
+
+struct Icons: View {
+    var body: some View {
+        Image(systemName: "doc.badge.plus")
+            .renderingMode(.original)
+            .resizable()
+            //.aspectRatio(contentMode: .fill)
+            //multicolor symbol
+            .scaledToFit()
+            //.font(.largeTitle)
+            //.font(.system(size: 200))
+            //.foregroundColor(Color.pink)
+            .frame(width: 200, height: 200)
+            .clipped() //fits the image
+    }
+}
+
+struct Icons_Previews: PreviewProvider {
+    static var previews: some View {
+        Icons()
+    }
+}
